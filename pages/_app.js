43dd6d521622580e38f433871d7578aff13e5c68
filipegbessaa <filipegbessa@gmail.com>
2021-08-08 19:40:32 -0300

    Setup basic project organization

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,22 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../src/theme/GlobalStyle';
 
 const theme = {
   colors: {
     primary: '#0070f3',
   },
-}
+};
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
---
+++ src/components/commons/Menu/styles/MenuWrapper.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const MenuWrapper = styled.nav`
+  font-family: 'Roboto', sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+MenuWrapper.Left = style.div`
+
+`
+MenuWrapper.center = style.div`
+
+`
+MenuWrapper.Right = style.div`
+
+`
